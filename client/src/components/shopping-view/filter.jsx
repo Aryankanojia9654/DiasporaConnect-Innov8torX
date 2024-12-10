4988282ffda2b3185c980aabfa2ab2546404dc2d
AkshayTiwari27 <131443493+AkshayTiwari27@users.noreply.github.com>
2024-12-10 18:35:57 +0530

    Update filter.jsx

--- client/src/components/shopping-view/filter.jsx
+++ client/src/components/shopping-view/filter.jsx
@@ -1,29 +1,27 @@
+import React, { Fragment } from "react";
 import { filterOptions } from "@/config";
-import { Fragment } from "react";
 import { Label } from "../ui/label";
 import { Checkbox } from "../ui/checkbox";
 import { Separator } from "../ui/separator";
 
 function ProductFilter({ filters, handleFilter }) {
   return (
-    <div className="bg-background rounded-lg shadow-sm">
+    <div className="bg-white rounded-lg shadow-sm border">
       <div className="p-4 border-b">
         <h2 className="text-lg font-extrabold">Filters</h2>
       </div>
-      <div className="p-4 space-y-4">
-        {Object.keys(filterOptions).map((keyItem) => (
-          <Fragment>
+      <div className="p-4 space-y-6">
+        {Object.entries(filterOptions).map(([keyItem, options]) => (
+          <Fragment key={keyItem}>
             <div>
-              <h3 className="text-base font-bold">{keyItem}</h3>
+              <h3 className="text-base font-bold capitalize">{keyItem}</h3>
               <div className="grid gap-2 mt-2">
-                {filterOptions[keyItem].map((option) => (
-                  <Label className="flex font-medium items-center gap-2 ">
+                {options.map((option) => (
+                  <Label key={option.id} className="flex font-medium items-center gap-2">
                     <Checkbox
                       checked={
                         filters &&
-                        Object.keys(filters).length > 0 &&
-                        filters[keyItem] &&
-                        filters[keyItem].indexOf(option.id) > -1
+                        filters[keyItem]?.includes(option.id)
                       }
                       onCheckedChange={() => handleFilter(keyItem, option.id)}
                     />
